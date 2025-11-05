6140f7167894afbc60b1fc329db12ae3aecfacdc
Luke Dowsett <luke.dowsett@lcp.uk.com>
2025-11-05 12:50:12 +0000

    Add validation import for Azure VM backup policy resource

--- provider/datasource_azure_service_accounts.go
+++ provider/datasource_azure_service_accounts.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
 
 // AzureServiceAccount represents an Azure service account from the Veeam API
